5e4078f1aabb87f5a04aa42346adb3e3cd7f14a9
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-04-29 11:56:25 -0300

    feat: add apikey config to report

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const reportController = require("./../controllers/reportController");
+const apiKeyMiddleware = require("../config/apiKey");
 
+router.use(apiKeyMiddleware);
 //Rota para gerar PDF
 router.get("/report/pdf", reportController.exportHeroPDF);
 
